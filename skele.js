36d65b96c788a7adb13bbd5651bc60e3c6b0df06
Julian <julian@Administrators-MacBook-Pro.local>
2023-04-23 19:36:47 -0400

    completed partners page + updated/sematic styling

--- skele.js
+++ skele.js
@@ -30,17 +30,3 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
-// // Auto Slide   if you need auto slide ,remove the commit "//"
-// var slideIndex = 0;
-// showSlides();
-// function showSlides() {
-//     var i;
-//    var slides = document.getElementsByClassName("mySlides");
-//    for (i = 0; i < slides.length; i++) {
-//        slides[i].style.display = "none";
-//    }
-//    slideIndex++;
-//    if (slideIndex > slides.length) { slideIndex = 1 }
-//    slides[slideIndex - 1].style.display = "block";
-//    setTimeout(showSlides, 5000); // Change image every 2 seconds
-// }
